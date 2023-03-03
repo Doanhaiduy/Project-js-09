3962189047bd457b7073676a201fef743542669b
Doanhaiduy <haiduytbt2k3@gmail.com>
2023-03-03 08:00:43 +0700

    init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,18 @@
-let content_1 = document.querySelector(".content-1");
-let content_2 = document.querySelector(".content-2");
-let container = document.querySelector(".container");
-content_1.addEventListener("mouseover", () => {
-    console.log(1);
-    container.classList.remove("active-2");
-    container.classList.add("active-1");
-});
+let list = document.querySelectorAll(".btn");
+let currentAudio;
 
-content_2.addEventListener("mouseover", () => {
-    console.log(1);
-    container.classList.remove("active-1");
-    container.classList.add("active-2");
+list.forEach((item) => {
+    item.addEventListener("click", () => {
+        const audio = item.querySelector("audio");
+        if (currentAudio) {
+            currentAudio.pause();
+        }
+        if (audio.paused) {
+            audio.play();
+            currentAudio = audio;
+        } else {
+            audio.pause();
+            currentAudio = null;
+        }
+    });
 });
